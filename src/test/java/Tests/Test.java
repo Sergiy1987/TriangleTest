dca524e2af82b2973426a4db98aa41f14a27c84e
Sergiy1987 <nedved198725@gmail.com>
2020-02-19 14:23:01 +0200

    add allure report

--- src/test/java/Tests/Test.java
+++ src/test/java/Tests/Test.java
@@ -3,11 +3,14 @@ package Tests;
  * Created by slobse01 on 07.02.2018.
  */
 
+import ru.yandex.qatools.allure.annotations.Step;
+
 import static Main.Figure_Triangle.*;
 import static org.testng.Assert.assertEquals;
 
 public class Test {
     @org.testng.annotations.Test
+    @Step
     public void TriangleHaveNotNegativeSides() {
         assertEquals(error, IdentifyTriangle(-15, 10, 13));
         assertEquals(error, IdentifyTriangle(20, -15, 22));
@@ -16,18 +19,21 @@ public class Test {
         assertEquals(error, IdentifyTriangle(-25, -12, -3));
     }
     @org.testng.annotations.Test
+    @Step
     public void TriangleIsEquilateral() {
         assertEquals(Equilateral, IdentifyTriangle(15, 15, 15));
         assertEquals(Equilateral, IdentifyTriangle(6, 6, 6));
         assertEquals(Equilateral, IdentifyTriangle(1, 1, 1));
     }
     @org.testng.annotations.Test
+    @Step
     public void TriangleIsIsosceles() {
         assertEquals(Isosceles, IdentifyTriangle(20, 20, 5));
         assertEquals(Isosceles, IdentifyTriangle(5, 9, 5));
         assertEquals(Isosceles, IdentifyTriangle(3, 10, 10));
     }
     @org.testng.annotations.Test
+    @Step
     public void TriangleIsScalene() {
         assertEquals(Scalene, IdentifyTriangle(15, 20, 10));
         assertEquals(Scalene, IdentifyTriangle(13, 14, 16));
